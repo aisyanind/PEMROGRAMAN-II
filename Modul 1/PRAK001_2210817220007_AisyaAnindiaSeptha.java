04c631b1dd4493b5d95b7650eb58c3abb34ab17b
aisyanind <2210817220007@mhs.ulm.ac.id>
2023-10-02 22:23:11 +0800

    ayam

---
+++ Modul 1/PRAK001_2210817220007_AisyaAnindiaSeptha.java
@@ -0,0 +1,7 @@
+public class PRAK001_2210817220007_AisyaAnindiaSeptha {
+    public static void main(String[] args) {
+        System.out.println("Selamat Pagi, Aisya Anindia Septha");
+        System.out.println("Selamat Siang, Aisya Anindia Septha");
+        System.out.println("Selamat Malam, Aisya Anindia Septha");
+    }
+}
--- Modul 1/PRAK101_2210817220007_AisyaAnindiaSeptha.java
+++
@@ -1,7 +0,0 @@
-public class PRAK101_2210817220007_AisyaAnindiaSeptha {
-    public static void main(String[] args) {
-        System.out.println("Selamat Pagi, Aisya Anindia Septha");
-        System.out.println("Selamat Siang, Aisya Anindia Septha");
-        System.out.println("Selamat Malam, Aisya Anindia Septha");
-    }
-}
